da35da160786c591c05d975c32c458993f358a98
Treppenhouse <schrobe@gmail.com>
2020-12-19 04:39:21 +0100

    add invalid game cheks, rate-sort function now None-safe

--- API_Test.py
+++ API_Test.py
@@ -1,6 +1,6 @@
-
 ###Anzahl und Liste der gespielten Liste
 from boardgamegeek import BGGClient
+
 bgg = BGGClient()
 
 playerName = "schrobe"
@@ -8,35 +8,42 @@ playerName = "schrobe"
 plays = bgg.plays(name=playerName)
 print("Anzahl gespielter Spiele: %d" % (len(plays)))
 
-
 # maps game IDs to the dictionary: {name, rating, rank}
 gameDict = {}
 for play in plays._plays:
     gameId = play.game_id
-    if(gameId == 1219 or gameId == 432): # these games have invalid data
+    if (gameId == 1219 or gameId == 432 or gameId == 862 or gameId == 4355):  # these games have invalid data
         continue
     game = bgg.game(game_id=gameId)
     name = game._name
     rating = game._data['stats']['bayesaverage']
     ranks = game._data['stats']['ranks']
     # For each game, different ranks are stored depending on a category. We want the rank in the category 'boardgame'
-    boardgameRank = next(x for x in ranks if x['name']=='boardgame')['value']
+    boardgameRank = next(x for x in ranks if x['name'] == 'boardgame')['value']
     # For each entry in our dictionary, the value is another dictionary that has entries for name, rating and rank
     gameDict[gameId] = {}
     valuesDict = gameDict[gameId]
     valuesDict['name'] = name
     valuesDict['rating'] = rating
     valuesDict['rank'] = boardgameRank
-    print(str(boardgameRank)+" : "+ str(rating)+ " : "+name) 
-	
+    print(str(boardgameRank) + " : " + str(rating) + " : " + name)
+
 # "Copy" the games dictionary into a list that is sorted by the 'rank'
+
+
+def get_rank(game_dict):
+    rank_ = game_dict[1]['rank']
+    if not rank_:
+        return 999999
+    return rank_
+
+
 # x[1] means: get the value of the dictionary
-sortedGames = sorted(gameDict.items(), key=lambda x: x[1]['rank'])
+sortedGames = sorted(gameDict.items(), key=lambda x: get_rank(x))
 
 for gameTuple in sortedGames:
     game = gameTuple[1]
-    print(str(game['rank'])+":\t"+ str(game['rating'])+ ":\t"+game['name']) 
-
+    print(str(game['rank']) + ":\t" + str(game['rating']) + ":\t" + game['name'])
 
 '''	
 ###Top 100 Liste, webscraped
